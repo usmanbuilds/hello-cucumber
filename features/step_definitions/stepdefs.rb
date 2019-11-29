09286a4ab19bbfe37deee5cb551d00cea460c8ca
Usman Riaz <uzzyman5@gmail.com>
2019-11-29 20:02:20 +0000

    Uses scenario outline to cover multiple test cases and avoid code duplication. Uses variables and examples.

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -7,10 +7,11 @@ module FridayStepHelper
     end
   end
 end
+
 World FridayStepHelper
 
-Given("today is Sunday") do
-  @today = 'Sunday'
+Given("today is {string}") do |given_day|
+  @today = given_day
 end
 
 When("I ask whether it's Friday yet") do
@@ -19,8 +20,4 @@ end
 
 Then("I should be told {string}") do |expected_answer|
   expect(@actual_answer).to eq(expected_answer)
-end
-
-Given("today is Friday") do
-  @today = 'Friday'
 end
